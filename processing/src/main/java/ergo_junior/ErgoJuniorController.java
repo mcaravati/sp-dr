7aa27a28f807f874f4787376ffaa0410d3080d33
Matteo Caravati <matteo.caravati@protonmail.com>
2023-07-04 17:35:59 +0200

    (feat) Added processing library skeleton

---
+++ processing/src/main/java/ergo_junior/ErgoJuniorController.java
@@ -0,0 +1,13 @@
+package ergo_junior;
+
+import processing.core.*;
+import oscP5.*;
+import netP5.*;
+
+public class ErgoJuniorController {
+    PApplet parent;
+
+    public ErgoJuniorController(PApplet parent) {
+        this.parent = parent;
+    }
+}
