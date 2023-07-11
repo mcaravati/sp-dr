d72f1c088181707db869f08ebb0086ba32261a22
Matteo Caravati <matteo.caravati@protonmail.com>
2023-07-11 11:12:28 +0200

    (feat) Added CLI arguments support

--- sp-dr-server/main.py
+++ sp-dr-server/main.py
@@ -2,22 +2,46 @@ from server.OSCServer import OSCServer
 from server.WebSocketServer import WebSocketServer
 from robot_control.SerialControl import SerialControl
 from robot_control.DummyRobotControl import DummyRobotControl
+import argparse
 
 if __name__ == "__main__":
-    robot_control = DummyRobotControl()
-    # robot_control = SerialControl(
-    #         port="/dev/ttyACM1",
-    #         baudrate=115200,
-    #         verbose=True
-    #     )
-
-
-    server = WebSocketServer(
-        robot_control=robot_control,
+    parser = argparse.ArgumentParser(
+        prog='SP//dr Server',
+        description='Start the linking server between platforms and the robot',
+        epilog='This script is distributed as free software and comes with no warranty'
     )
-    # server = OSCServer(
-    #     robot_control=robot_control,
-    # )
+
+    parser.add_argument(
+        '--dummy',
+        action='store_true',
+        help='Use a dummy robot control instead of a serial connection'
+    )
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
+    parser.add_argument('--serial-port', type=str, help='Serial port to connect to')
+    parser.add_argument('-p', '--platform', type=str, help='Platform to connect to', required=True)
+
+    args = parser.parse_args()
+    robot_control = None
+    server = None
+
+    if args.dummy:
+        robot_control = DummyRobotControl()
+    else:
+        robot_control = SerialControl(
+            port=args.serial_port,
+            baudrate=115200,
+            verbose=args.verbose
+        )
+
+    if args.platform in ['processing', 'puredata']:
+        server = OSCServer(
+            robot_control=robot_control,
+        )
+    elif args.platform in ['p5']:
+        server = WebSocketServer(
+            robot_control=robot_control,
+        )
+
     server.start()
 
     while True:
@@ -25,3 +49,5 @@ if __name__ == "__main__":
             break
 
     server.stop()
+else:
+    raise Exception("This script should be run as main")
